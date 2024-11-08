2087029dfc07561c9b7a66c085e9ce37842b7b0f
Mateusz Moniowski <mateusz.moniowski@microsoft.wsei.edu.pl>
2024-11-08 12:57:03 +0100

    add link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import { data } from './module-data'
 import PersonProfile from './components/PersonProfile';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 
 function App() {
+  const [alert, setAlert] = useState("")
+
   return (
     <div className='container'>
       <h1>Osoby</h1>
+      <a href="http://backend:4000">Backend</a>
       <div className='row g-3'>
         {data.map((person, index) => <PersonProfile key={person.id} person={person} />)}
       </div>
--- src/module-data.js
+++ src/module-data.js
@@ -1,92 +1,92 @@
 export const data = [
 {
 	id: "1",
-	name: "Janusz",
+	name: "Stanisław",
 	eyeColor: "brown",
-	birthDate: "02/01/2000"
+	birthDate: "15/10/1985"
 },
 {
 	id: "2",
-	name: "Janusz",
-	eyeColor: "gray",
-	birthDate: "16/03/1997"
+	name: "Jarek",
+	eyeColor: "brown",
+	birthDate: "28/03/2005"
 },
 {
 	id: "3",
-	name: "Kornelia",
+	name: "Monika",
 	eyeColor: "yellow",
-	birthDate: "05/08/1972"
+	birthDate: "09/03/1988"
 },
 {
 	id: "4",
-	name: "Janusz",
-	eyeColor: "yellow",
-	birthDate: "17/04/1976"
+	name: "Jarek",
+	eyeColor: "blue",
+	birthDate: "03/11/2001"
 },
 {
 	id: "5",
-	name: "Kornelia",
-	eyeColor: "green",
-	birthDate: "02/12/2001"
+	name: "Stanisław",
+	eyeColor: "yellow",
+	birthDate: "07/04/1993"
 },
 {
 	id: "6",
-	name: "Magda",
-	eyeColor: "blue",
-	birthDate: "20/06/1970"
+	name: "Jarek",
+	eyeColor: "brown",
+	birthDate: "02/07/1993"
 },
 {
 	id: "7",
-	name: "Magda",
-	eyeColor: "green",
-	birthDate: "31/10/1974"
+	name: "Kornelia",
+	eyeColor: "blue",
+	birthDate: "20/03/1987"
 },
 {
 	id: "8",
-	name: "Jarek",
-	eyeColor: "yellow",
-	birthDate: "20/10/1995"
+	name: "Stanisław",
+	eyeColor: "green",
+	birthDate: "17/01/2004"
 },
 {
 	id: "9",
 	name: "Jarek",
-	eyeColor: "green",
-	birthDate: "06/07/1983"
+	eyeColor: "yellow",
+	birthDate: "13/11/1982"
 },
 {
 	id: "10",
-	name: "Stanisław",
-	eyeColor: "gray",
-	birthDate: "12/10/1986"
+	name: "Magda",
+	eyeColor: "blue",
+	birthDate: "12/08/1977"
 },
 {
 	id: "11",
-	name: "Janusz",
-	eyeColor: "brown",
-	birthDate: "01/05/2002"
+	name: "Monika",
+	eyeColor: "blue",
+	birthDate: "24/09/1984"
 },
 {
 	id: "12",
-	name: "Magda",
-	eyeColor: "gray",
-	birthDate: "06/09/1998"
+	name: "Janusz",
+	eyeColor: "yellow",
+	birthDate: "24/05/1993"
 },
 {
 	id: "13",
 	name: "Stanisław",
 	eyeColor: "blue",
-	birthDate: "11/04/1974"
+	birthDate: "28/01/1997"
 },
 {
 	id: "14",
-	name: "Monika",
-	eyeColor: "yellow",
-	birthDate: "01/02/2004"
+	name: "Kornelia",
+	eyeColor: "blue",
+	birthDate: "18/08/1982"
 },
 {
 	id: "15",
-	name: "Stanisław",
-	eyeColor: "brown",
-	birthDate: "24/11/1994"
+	name: "Magda",
+	eyeColor: "yellow",
+	birthDate: "04/04/1971"
 },
 ];
